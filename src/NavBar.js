167b4e0766ac445253fa4ab0825eada611d83992
KaranDasA1 <141057770+KaranDasA1@users.noreply.github.com>
2024-06-03 12:53:15 +0530

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,14 +1,18 @@
-import "./NavBar.css" ;
-function NavBar(){
-    return(
-        <nav>
-            <ul class="navbar">
-                <li class="nav-item"><a href="#">Home</a></li>
-                <li class="nav-item"><a href="#">About</a></li>
-                <li class="nav-item"><a href="#">Project</a></li>
-                <li class="nav-item"><a href="#">Resume</a></li>
+import './NavBar.css';
+import logo from './assets/logo.png'; // Import the logo
+
+function NavBar() {
+    return (
+        <div className="navbar">
+            <img src={logo} alt="Logo" className="nav-logo" /> {logo.png}
+            <ul className="nav-items">
+                <li className="nav-item"><a href="#">Home</a></li>
+                <li className="nav-item"><a href="#">About</a></li>
+                <li className="nav-item"><a href="#">Projects</a></li>
+                <li className="nav-item"><a href="#">Resume</a></li>
             </ul>
-        </nav>
-    )
+        </div>
+    );
 }
+
 export default NavBar;
