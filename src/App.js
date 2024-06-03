cc1aebcfc844640822179825914292cbacee9881
KaranDasA1 <141057770+KaranDasA1@users.noreply.github.com>
2024-06-03 12:52:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-
 import './App.css';
 import NavBar from './NavBar.js';
 import Typing from './Typing.js';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <NavBar />
+      <NavBar />
+      <div className="content">
+        <div className="intro-text">
+          I am <span className="highlight">Karan Das</span>
+        </div>
         <Typing
           text={[
-            "I am a Programmer",
+            "A Programmer",
             "Problem Solver",
             "Web Developer",
           ]}
           typingSpeed={100}
           deletingSpeed={50}
-          duration={1000}
         />
-      </header>
+      </div>
     </div>
   );
 }
